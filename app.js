65404a9203c73b97e79942ad5f5c5555c8a4731c
King Pra <kingi.pra@gmail.com>
2018-11-14 21:41:52 -0500

    link to email recieved message

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-function login() {
+// checks to make sure input values on login page are not empty. Alerts user they are signed or if they need to fill out input field.
+function validate(val) {
   event.preventDefault();
+  val === "login" ? login() : email();
+}
 
+function login() {
   const name = document.querySelector("#user-name").value;
 
   const pass = document.querySelector("#password").value;
-  console.log(pass);
 
   name.length > 0 && pass.length > 0
     ? alert(`You are now logged in as ${name}`)
     : alert("Please enter a a valid name and/or password.");
+}
+
+function email() {
+  const email = document.querySelector(".email").value;
+  console.log(email.length);
+
+  email.length > 1 ? window.location.replace("./email-sent.html") : "";
 }
